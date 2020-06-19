7b697fb75b311e5a38309dd1e950c167c4e04f4b
jalayna <jalaynatipantiza@gmail.com>
2020-06-19 15:46:01 +0000

    refactoring code, updating readme file

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Tweeter is a simple, single-page Twitter clone.
 
-This repository is the starter code for the project: Students will fork and clone this repository, then build upon it to practice their HTML, CSS, JS, jQuery and AJAX front-end skills, and their Node, Express and MongoDB back-end skills.
+This project focused on using  HTML, CSS, JS, jQuery and AJAX on the front-end. It has a response of design targeted for mobile and desktop users. 
 
 ## Getting Started
 
@@ -15,3 +15,7 @@ This repository is the starter code for the project: Students will fork and clon
 
 - Express
 - Node 5.10.x or above
+- Body-parser
+- Chance
+- express
+- md5
--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,74 +1,76 @@
 $(document).ready(function() {
 
   //Displays error slide up and down
-  $('#new-post').on('submit', function(event) {  
+  $('#new-post').on('submit', function(event) {
+
     let data = $(this).serialize();
     let decodedData = decodeURIComponent(data).slice(5);
     event.preventDefault();
 
-    $("#error-message").slideUp()
+    $("#error-message").slideUp();
     
     if (decodedData.length > 140) {
-      $("#error-messages").empty();
-      $("#error-messages").prepend(`
-      <div id="error-message">
-      <b>🚫Easy there pal, tweet is too long. Plz respect our arbitrary limit of 140 characters🚫</b>
-      </div>`)
-      if ( $( "#error-message" ).first().is( ":hidden" ) ) {
-          $( "#error-message" ).slideDown();
-        }   
-    } else if (!decodedData) {
-      $("#error-messages").empty();
-      $("#error-messages").prepend(`
-      <div id="error-message">
-      <b>🚫 Easy there pal, you didn't enter anything 🚫</b>
-      </div>`)
+      const message = '🚫Easy there pal, tweet is too long. Plz respect our arbitrary limit of 140 characters🚫'
+     renderAlert(message);
 
-      if ( $( "#error-message" ).first().is( ":hidden" ) ) {
-          $( "#error-message" ).slideDown();
-      }
+    } else if (!decodedData) {
+      const message = "🚫 Easy there pal, you didn't enter anything 🚫"
+      renderAlert(message);
+
     } else {
       $.post('/tweets/', data, function() {
-        loadtweets();   
+        loadtweets();
         $("textarea").val('').change();
-        $(".counter").val('140').change();   
+        $(".counter").val('140').change();
       });
     }
   });
-  
-  //button event listener 
-  $(".double-down").on("click", function() {
-    if($( ".new-tweet" ).first().is( ":hidden" )) {
-      $( ".new-tweet" ).slideDown();
-      $( "textarea" ).focus();
-    } else {
-      $( ".new-tweet" ).slideUp();
+
+  //Renders error message function 
+  const renderAlert = (message)=>{
+    $("#error-messages").empty();
+    $("#error-messages").prepend(`
+    <div id="error-message">
+    <b>${message}</b>
+    </div>`);
+    if ($("#error-message").first().is(":hidden")) {
+      $("#error-message").slideDown();
     }
-  })
+  }
+  
+  //create new tweet button
+  $(".double-down").on("click", function() {
+    if ($(".new-tweet").first().is(":hidden")) {
+      $(".new-tweet").slideDown();
+      $("textarea").focus();
+    } else {
+      $(".new-tweet").slideUp();
+    }
+  });
   
   const loadtweets = () => {
     $.getJSON('/tweets/', function(data) {
-      $('#tweets-container').empty()
-      renderTweets(data);
-      
+      $('#tweets-container').empty();
+      renderTweets(data); 
     });
   };
 
   const renderTweets = function(tweets) {
-    $('#container').empty()   
+    $('#container').empty();
     for (const tweet of tweets) {
       const newTweet = createTweetElement(tweet);
       $('#tweets-container').prepend(newTweet);
-    }    
+    }
   };
     
+  //layout for each tweet
   const createTweetElement = function(tweet) {
     const name = tweet.user.name;
     const avatar = tweet.user.avatars;
     const handle = tweet.user.handle;
     const text = tweet.content.text;
     const createdAt = moment(tweet.created_at).fromNow();
-      
+     
     const tweetArticle = `
       <article> 
       <header class="tweet-header">
@@ -92,11 +94,12 @@ $(document).ready(function() {
     return tweetArticle;
   };
 
+  //Preventing XSS with Escaping
   const escape =  function(str) {
     let div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
-  }
+  };
     
   loadtweets();
 });
